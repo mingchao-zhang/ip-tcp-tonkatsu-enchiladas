a36b0556b820c33894fc80f8c5966b127c94cc60
Rutvik Saptarshi <rutvik@pop-os.localdomain>
2022-10-16 16:06:18 -0400

    added go mod

--- pkg/fwdtable/fwdtable.go
+++ pkg/fwdtable/fwdtable.go
@@ -2,20 +2,28 @@ package fwdtable
 
 import (
 	"fmt"
+	"log"
 	"sync"
+
+	"github.com/google/netstack/tcpip/header"
+	"golang.org/x/net/ipv4"
 )
 
 type HandlerFunc = func([]byte, []interface{})
 
 type fwdTable struct {
-	table map[string]string
-	lock  *sync.RWMutex
+	// net.IP
+	myIntefaces map[string]bool
+	table       map[string]string
+	lock        *sync.RWMutex
 	// uint8 is the protocol number for the application
+	// try sync map
 	applications map[uint8]HandlerFunc
 }
 
 var fwdtable fwdTable
 
+// Takes a list of links
 func InitFwdTable() {
 	fwdtable.table = make(map[string]string)
 	fwdtable.lock = new(sync.RWMutex)
@@ -29,7 +37,10 @@ func AddRecord(ip string, nextHop string) {
 }
 
 func RegisterApplication(protocolNum uint8, hf HandlerFunc) {
+	fwdtable.lock.Lock()
+	defer fwdtable.lock.Unlock()
 
+	fwdtable.applications[protocolNum] = hf
 }
 
 func RemoveRecord(ip string) {
@@ -39,6 +50,10 @@ func RemoveRecord(ip string) {
 	delete(fwdtable.table, ip)
 }
 
+func GetRecord(ip string) (nextHop string) {
+	return ""
+}
+
 func Print() {
 	fwdtable.lock.RLock()
 	defer fwdtable.lock.RUnlock()
@@ -46,4 +61,21 @@ func Print() {
 	for k, v := range fwdtable.table {
 		fmt.Printf("key[%s] value[%s]\n", k, v)
 	}
+}
+
+func HandlePacket(hdr *ipv4.Header, message []byte) {
+	hdrBytes, err := hdr.Marshal()
+	if err != nil {
+		log.Println("Unable to marshal header in HandlePacket 🙀")
+		return
+	}
+
+	// check checksum
+	if hdr.Checksum != int(header.Checksum(hdrBytes, 0)) {
+		log.Println("Received packet with invalid checksum 😵")
+		return
+	}
+
+	// check if destination is one of the interfaces on this node
+
 }
