a3e3f56c8dc66b49f5df2a9e0b9ea37fb3c59fa6
Mingchao Zhang <zmcmarkzhang@gmail.com>
2022-10-16 16:53:14 -0400

    transport layer

---
+++ pkg/transport/transport.go
@@ -0,0 +1,15 @@
+package transport
+
+import (
+	"net"
+	// "github.com/google/netstack/tcpip/header"
+	// "golang.org/x/net/ipv4"
+)
+
+func Recv(conn net.UDPConn, listenChan *chan []byte) {
+
+}
+
+func Send(msg []byte, conn net.UDPConn) {
+
+}
