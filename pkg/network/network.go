4adcb5cde81303cfafe81be2463239c8ffdfc8cc
Rutvik Saptarshi <rutvik@pop-os.localdomain>
2022-10-16 16:52:28 -0400

    pushing fwdtable

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -1,4 +1,4 @@
-package fwdtable
+package network
 
 import (
 	"fmt"
@@ -22,46 +22,46 @@ type FwdTable struct {
 }
 
 // Takes a list of links
-func InitFwdTable() {
-	fwdtable.table = make(map[string]string)
-	fwdtable.lock = new(sync.RWMutex)
+func (ft *FwdTable) InitFwdTable() {
+	ft.table = make(map[string]string)
+	ft.lock = new(sync.RWMutex)
 }
 
-func AddRecord(ip string, nextHop string) {
-	fwdtable.lock.Lock()
-	defer fwdtable.lock.Unlock()
+func (ft *FwdTable) AddRecord(ip string, nextHop string) {
+	ft.lock.Lock()
+	defer ft.lock.Unlock()
 
-	fwdtable.table[ip] = nextHop
+	ft.table[ip] = nextHop
 }
 
-func RegisterApplication(protocolNum uint8, hf HandlerFunc) {
-	fwdtable.lock.Lock()
-	defer fwdtable.lock.Unlock()
+func (ft *FwdTable) RegisterApplication(protocolNum uint8, hf HandlerFunc) {
+	ft.lock.Lock()
+	defer ft.lock.Unlock()
 
-	fwdtable.applications[protocolNum] = hf
+	ft.applications[protocolNum] = hf
 }
 
-func RemoveRecord(ip string) {
-	fwdtable.lock.Lock()
-	defer fwdtable.lock.Unlock()
+func (ft *FwdTable) RemoveRecord(ip string) {
+	ft.lock.Lock()
+	defer ft.lock.Unlock()
 
-	delete(fwdtable.table, ip)
+	delete(ft.table, ip)
 }
 
-func GetRecord(ip string) (nextHop string) {
+func (ft *FwdTable) GetRecord(ip string) (nextHop string) {
 	return ""
 }
 
-func Print() {
-	fwdtable.lock.RLock()
-	defer fwdtable.lock.RUnlock()
+func (ft *FwdTable) Print() {
+	ft.lock.RLock()
+	defer ft.lock.RUnlock()
 
-	for k, v := range fwdtable.table {
+	for k, v := range ft.table {
 		fmt.Printf("key[%s] value[%s]\n", k, v)
 	}
 }
 
-func HandlePacket(hdr *ipv4.Header, message []byte) {
+func (ft *FwdTable) HandlePacket(hdr *ipv4.Header, message []byte) {
 	hdrBytes, err := hdr.Marshal()
 	if err != nil {
 		log.Println("Unable to marshal header in HandlePacket 🙀")
@@ -76,17 +76,17 @@ func HandlePacket(hdr *ipv4.Header, message []byte) {
 
 	// check if destination is one of the interfaces on this node
 	destIP := hdr.Dst.String()
-	_, ok := fwdtable.myInterfaces[destIP]
+	_, ok := ft.myInterfaces[destIP]
 	if ok {
 		// we are the destination, call the handler for the appropriate application
 	} else {
 		// not the destination, forward to next hop
 		// what do we do if we don't know a next hop for this destination???
-		nextHop, ok := fwdtable.table[destIP]
+		nextHop, ok := ft.table[destIP]
 		if !ok {
 			log.Println("Don't know how to get to this destination 🤷🏾")
 		}
-
+		fmt.Println(nextHop)
 		// we should probably make the node stuff a separate package
 		node.Send()
 	}
