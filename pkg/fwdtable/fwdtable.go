310a76a3e7f4619e371c821a12649b7c8a78b2e9
Rutvik Saptarshi <rutvik@pop-os.localdomain>
2022-10-16 16:45:59 -0400

    creating new branch

--- pkg/fwdtable/fwdtable.go
+++ pkg/fwdtable/fwdtable.go
@@ -11,17 +11,15 @@ import (
 
 type HandlerFunc = func([]byte, []interface{})
 
-type fwdTable struct {
+type FwdTable struct {
 	// net.IP
-	myIntefaces map[string]bool
-	table       map[string]string
-	lock        *sync.RWMutex
+	myInterfaces map[string]bool
+	table        map[string]string
+	lock         *sync.RWMutex
 	// uint8 is the protocol number for the application
 	// try sync map
 	applications map[uint8]HandlerFunc
 }
-
-var fwdtable fwdTable
 
 // Takes a list of links
 func InitFwdTable() {
@@ -77,5 +75,19 @@ func HandlePacket(hdr *ipv4.Header, message []byte) {
 	}
 
 	// check if destination is one of the interfaces on this node
+	destIP := hdr.Dst.String()
+	_, ok := fwdtable.myInterfaces[destIP]
+	if ok {
+		// we are the destination, call the handler for the appropriate application
+	} else {
+		// not the destination, forward to next hop
+		// what do we do if we don't know a next hop for this destination???
+		nextHop, ok := fwdtable.table[destIP]
+		if !ok {
+			log.Println("Don't know how to get to this destination 🤷🏾")
+		}
 
+		// we should probably make the node stuff a separate package
+		node.Send()
+	}
 }
