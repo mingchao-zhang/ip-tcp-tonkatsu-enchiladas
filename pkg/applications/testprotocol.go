775241fb8e3c18e6eb4d325b7d73052bab7038b5
Mingchao Zhang <zmcmarkzhang@gmail.com>
2022-10-23 20:09:55 -0400

    test protocol handler done

--- pkg/applications/testprotocol.go
+++ pkg/applications/testprotocol.go
@@ -12,8 +12,7 @@ const (
 )
 
 func TestProtocolHandler(packet []byte, params []interface{}) {
-	// TODO: get hdr from params
-	var hdr ipv4.Header
+	hdr := params[0].(*ipv4.Header)
 	fmt.Println("---Node received packet!---")
 	fmt.Printf("source IP      : %s\n", hdr.Src.String())
 	fmt.Printf("destination IP : %s\n", hdr.Dst.String())
