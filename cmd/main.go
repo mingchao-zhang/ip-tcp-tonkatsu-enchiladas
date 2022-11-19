a5b0629ff409022f4cb146ec90de65be46d38cf0
Mingchao Zhang <zmcmarkzhang@gmail.com>
2022-11-19 16:53:58 -0500

    heap working

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"bufio"
+	"container/heap"
 	"fmt"
+	"ip/pkg/applications/tcp"
 	"log"
 	"os"
 )
 
 func main() {
+	// Some items and their priorities.
+	items := map[string]int{
+		"banana": 3, "apple": 2, "pear": 4,
+	}
+
+	// Create a priority queue, put the items in it, and
+	// establish the priority queue (heap) invariants.
+	pq := make(tcp.PriorityQueue, len(items))
+	i := 0
+	for value, priority := range items {
+		pq[i] = &tcp.Item{
+			Value:    value,
+			Priority: priority,
+			Index:    i,
+		}
+		i++
+	}
+	heap.Init(&pq)
+
+	// Insert a new item and then modify its priority.
+	item := &tcp.Item{
+		Value:    "orange",
+		Priority: 1,
+	}
+	heap.Push(&pq, item)
+	pq.Update(item, item.Value, 5)
+
+	// Take the items out; they arrive in decreasing priority order.
+	for pq.Len() > 0 {
+		item := heap.Pop(&pq).(*tcp.Item)
+		fmt.Printf("%.2d:%s ", item.Priority, item.Value)
+	}
+
+	return
+
 	if len(os.Args) != 2 {
 		log.Println("Incorrect number of arguments. Correct usage: node <linksfile>")
 		os.Exit(1)
